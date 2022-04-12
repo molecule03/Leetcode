4932b3638d8c0f8ab9aaad25af9b40d5d2456be4
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-04-13 02:18:59 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (90.21%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -1,41 +1,44 @@
 class Solution {
     public void gameOfLife(int[][] board) {
+        
+// This is to travers in all 8 directions...         
         int[] dx = {0,1,1,1,0,-1,-1,-1};
         int[] dy = {1,1,0,-1,-1,-1,0,1};
         
        
-        for(int c=0; c<board.length; c++){
-            for(int r=0; r<board[0].length; r++){
-                int curL = 0;
-                   // System.out.println(c+" "+r);
+        for(int col=0; col<board.length; col++){
+            for(int row=0; row<board[0].length; row++){
+                int curLength = 0;
+                
+                //this for loop is to check all 8 directions
                 for(int i=0; i<8; i++){
                     
-                    int curX = c+dx[i];
-                    int curY = r+dy[i];
+                    int curX = col+dx[i];
+                    int curY = row+dy[i];
                     
                     if(isValidNeigh(curX,curY, board) && Math.abs(board[curX][curY]) == 1)
-                        curL++;
+                        curLength++;
                 }
-                // System.out.println(curL);
                 
-                if(board[c][r] == 1 && (curL<2 || 3<curL))
-                    board[c][r] = -1;
-                else if(board[c][r] == 0 && curL == 3)
-                    board[c][r] = 2;
+                if(board[col][row] == 1 && (curLength<2 || 3<curLength))
+                    board[col][row] = -1;
+                else if(board[col][row] == 0 && curLength == 3)
+                    board[col][row] = 2;
             }
         }
         
-        for(int c=0; c<board.length; c++){
-            for(int r=0; r<board[0].length; r++){
-                if(board[c][r] >= 1)
-                    board[c][r] = 1;
+        for(int col=0; col<board.length; col++){
+            for(int row=0; row<board[0].length; row++){
+                if(board[col][row] >= 1)
+                    board[col][row] = 1;
                 else 
-                    board[c][r] = 0;
+                    board[col][row] = 0;
             }
         }
         
     }
     
+// This Method is to simply check index.outOfBound..   we directly write this is main method but to avoid mess we are writng like this..
     private boolean isValidNeigh(int x, int y, int[][] board){
         if(x < 0 || x >= board.length || y < 0 || y >= board[0].length)
             return false;
