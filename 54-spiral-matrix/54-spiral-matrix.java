95f42f0e4d6315b2622e8fd4cbe7af92c01bca45
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-04-14 01:36:28 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (82.73%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -0,0 +1,58 @@
+class Solution {
+    boolean[][] vis;
+    public List<Integer> spiralOrder(int[][] res) {
+        
+        vis = new boolean[res.length][res[0].length];
+        List<Integer> list = new ArrayList<>();
+        
+        int rowB = 0;
+        int rowE = res.length-1;
+        int colB = 0;
+        int colE = res[0].length-1;
+
+        while(rowB <= rowE && colB <= colE){ 
+            
+// for right traverse
+            for(int i=colB; i<=colE; i++){
+                if(!isVis(rowB,i)){
+                list.add(res[rowB][i]); 
+                vis[rowB][i] = true;
+                }
+            }
+            rowB++;
+            
+// for down traverse   
+            for(int i=rowB; i<=rowE; i++){
+                  if(!isVis(i,colE)){
+                list.add(res[i][colE]); 
+                vis[i][colE] = true;
+                  }
+            }
+            colE--;
+            
+// for left traverse
+            for(int i=colE; i>=colB; i--){
+                  if(!isVis(rowE,i)){
+                list.add(res[rowE][i]);
+                vis[rowE][i] = true;
+                  }
+            }
+            rowE--;
+            
+// for up traverse\
+            for(int i=rowE; i>=rowB; i--){
+                  if(!isVis(i,colB)){
+                list.add(res[i][colB]);
+                vis[i][colB] = true;
+                  }
+            }
+            colB++;
+        } 
+        
+        return list;
+    }
+    
+    private boolean isVis(int x, int y){
+        return vis[x][y];
+    }
+}
