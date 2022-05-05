8d003322cc31d5345c67fbd57d7fdb606d01ba48
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-05-05 23:49:20 +0530

    Time: 99 ms (5.24%), Space: 101.9 MB (5.05%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,30 +1,33 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
         
-        
+// Marking all the freq of array
         Map<Integer,Integer> map = new HashMap<>();
         for(int x : nums)
             map.put(x,map.getOrDefault(x,0)+1);
         
         
-        
         int count = 0;
         for(int x : nums){
-            // System.out.println(map);
+
+// If both the elements are present in the array whose sum==k  increment count++ and removing then form array
             if(map.containsKey(k-x) && map.containsKey(x)){
                 
                 if(x == k-x && map.get(x) < 2)
                     continue;
                 
+                
                 if(map.get(x) > 1)
                     map.put(x, map.get(x)-1);
                 else
                     map.remove(x);
+                 
                 
                 if(map.get(k-x) > 1)
                     map.put(k-x, map.get(k-x)-1);
                 else
                     map.remove(k-x);
+                
                 
                 count++;
             }
